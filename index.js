1fbbcdaaa7d98d208971086d43f51b8825aeb608
mowuj <mowuj92@gmail.com>
2022-05-27 09:50:44 +0600

    booking post api

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
+const stripe = require('stripe')('sk_test_51L3HzCFICgNuMVETcGEvtXvTSgk9VJqgXRNe4BAK7H1BD0Gmtq0GiMcNKixBg3TK6p3kS34S4jEtN03ShY05hYgC00vhKKsOAi')
 const { MongoClient, ServerApiVersion,ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express()
@@ -9,33 +10,35 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json())
 
-function verifyJWT(req, res, next) {
-    const authHeader = req.headers.authorization;
-    if (!authHeader) {
-        return res.status(401).send({message:'unauthorized access'})
-    }
-    const token = authHeader.split(' ')[1];
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-        if (err) {
-            return res.status(403).send({message:'Forbidden access'})
-        }
-        console.log('decoded', decoded);
-        req.decoded = decoded;
-        next();
-    })
-    
-    
-}
+
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jjbqr.mongodb.net/?retryWrites=true&w=majority"`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-
+function verifyJWT(req, res, next) {
+  const authHeaders = req.headers.authorization;
+  if (!authHeaders) {
+    return res.status(401).send({message:'UnAuthorized access'})
+    
+  }
+  const token = authHeaders.split(' ')[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+    if (err) {
+      return res.status(403).send({message:'Forbidden access'})
+    }
+    req.decoded = decoded;
+    next();
+  })
+}
 async function run() {
   try {
     await client.connect();
     const serviceCollection = client.db('lucky_tools').collection('services');
     const userCollection = client.db('lucky_tools').collection('users');
     const bookingCollection = client.db('lucky_tools').collection('bookings');
+    const paymentCollection = client.db('lucky_tools').collection('payments');
+
+    
+
     app.get('/service', async (req, res) => {
         const query = {};
         const cursor = serviceCollection.find(query);
@@ -45,18 +48,31 @@ async function run() {
     app.get('/user', async (req, res) => {
         const users = await userCollection.find().toArray();
         res.send(users);
-      });
-
+    });
+    
+      app.get('/booking', async(req, res) =>{
+      const customer = req.query.customer;
+        
+      const query = {customer: customer};
+      const bookings = await bookingCollection.find(query).toArray();
+      return res.send(bookings);
+       
+      })
+    app.get('/booking/:id', async(req, res) =>{
+      const id = req.params.id;
+      const query = {_id: ObjectId(id)};
+      const booking = await bookingCollection.findOne(query);
+      res.send(booking);
+    })
     app.post('/booking', async (req, res) => {
         const booking = req.body;
         const query = { service: booking.service, price: booking.price, customer: booking.customer,customerName:booking.customerName }
         const exists = await bookingCollection.findOne(query)
-        // if (exists) {
-        //   return res.send({ success: false, booking: exists })
-        // }
+        
         const result = await bookingCollection.insertOne(booking)
         return res.send({ success: true, result })
-      });
+    });
+   
 
   }
   finally {
