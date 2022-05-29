ee0440171dbc7fedac3134b4750cdf403f14200a
mowuj <mowuj92@gmail.com>
2022-05-29 12:57:14 +0600

    solve database problem

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
+require('dotenv').config();
 // const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
 const { MongoClient, ServerApiVersion,ObjectId } = require('mongodb');
-require('dotenv').config();
+
 const app = express()
 const port = process.env.PORT || 5000;
 
@@ -12,24 +13,23 @@ app.use(express.json())
 
 
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jjbqr.mongodb.net/?retryWrites=true&w=majority"`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jjbqr.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+// fun
 function verifyJWT(req, res, next) {
-    const authHeader = req.headers.authorization;
-    if (!authHeader) {
-        return res.status(401).send({message:'unauthorized access'})
+  const authHeaders = req.headers.authorization;
+  if (!authHeaders) {
+    return res.status(401).send({message:'UnAuthorized access'})
+    
+  }
+  const token = authHeaders.split(' ')[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+    if (err) {
+      return res.status(403).send({message:'Forbidden access'})
     }
-    const token = authHeader.split(' ')[1];
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-        if (err) {
-            return res.status(403).send({message:'Forbidden access'})
-        }
-        console.log('decoded', decoded);
-        req.decoded = decoded;
-        next();
-    })
-    
-    
+    req.decoded = decoded;
+    next();
+  })
 }
 async function run() {
   try {
@@ -38,7 +38,10 @@ async function run() {
     const userCollection = client.db('lucky_tools').collection('users');
     const bookingCollection = client.db('lucky_tools').collection('bookings');
     const paymentCollection = client.db('lucky_tools').collection('payments');
-
+    app.get('/user', async (req, res) => {
+        const users = await userCollection.find().toArray();
+        res.send(users);
+      });
     app.put('/user/:email', async (req, res) => {
       const email = req.params.email;
       const user = req.body;
@@ -95,20 +98,21 @@ async function run() {
       res.send(booking);
     })
 
-    app.get('/booking', async(req, res) =>{
-        const customer = req.query.customer;
+    app.get('/booking',verifyJWT, async (req, res) => {
+      const customer = req.query.customer;
         
-        const decodedEmail = req.decoded.email;
-        if (customer === decodedEmail) {
-          const query = {customer: customer};
-      const bookings = await bookingCollection.find(query).toArray();
-      return res.send(bookings);
-        }
-        else {
-          return res.status(403).send({message:'Forbidden access'})
-        }
+      const decodedEmail = req.decoded.email;
+      if (customer === decodedEmail) {
+        const query = { customer: customer };
+        const bookings = await bookingCollection.find(query).toArray();
+        return res.send(bookings);
+      }
+      else {
+        return res.status(403).send({ message: 'Forbidden access' })
+      }
       
-      })
+    });
+   
     app.post('/booking', async (req, res) => {
         const booking = req.body;
         const query = { service: booking.service, price: booking.price, customer: booking.customer,customerName:booking.customerName }
